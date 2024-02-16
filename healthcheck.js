c0f110a25dc0780a7f618922afa9c8fbdb4b1d07
Azim Baji Krishna <azimbaji19@gmail.com>
2024-02-16 18:44:15 +0530

    possible fix

--- healthcheck.js
+++ healthcheck.js
@@ -26,7 +26,7 @@ async function checkWebsite() {
           '--no-sandbox',
           '--disable-setuid-sandbox',
         ],
-        executablePath: "/usr/bin/google-chrome-stable",
+        executablePath: "/opt/chrome/headless/chrome",
       });
       const page = await browser.newPage();
 
